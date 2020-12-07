69ef99720221b4eec999e967155ae6a5e6d45565
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-12-07 13:13:04 -0300

    Create Variables.kt

---
+++ variables_and_datatypes/Variables.kt
@@ -0,0 +1,19 @@
+class Main {
+
+}
+
+fun main(args: Array<String>) {
+   /*val wholeNumber = 3
+    val bigNumber = 3L
+    val preciseDecimal = 3.33
+    val decimal = 3.33f
+    val iLoveVegetables = false
+    val iLovePizza = true
+    val iAmALetter = 'a' */
+
+    val x = 3f / 4f
+    println("The value of x is $x")
+
+    val X = 3 * 4
+    println("The value of x is $X")
+}
