0c26484500f5df07b3bae2340b96d6b17de18e53
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-12-09 21:52:15 -0300

    Create Main.kt

---
+++ classes/Main.kt
@@ -0,0 +1,21 @@
+class Main {
+
+}
+
+fun main(args: Array<String>) {
+  val dog = Animal("Dog")
+}
+fun Int.isOdd(): Boolean {
+    return this % 2 == 1
+}
+
+fun isEven(number: Int = 10): Boolean {
+    return number % 2 == 0
+}
+
+fun print10Numbers() {
+    for (i in 1..10) {
+        println(i)
+    }
+}
+
